dfb5eee8b3c56286577af30e30cc7b7ed47b9537
Faizan Shaikh <faizansheikh8209@gmail.com>
2025-01-10 16:08:34 +0530

    card-flip-ui

--- company/src/app/testonomial/page.tsx
+++ company/src/app/testonomial/page.tsx
@@ -60,100 +60,167 @@ export default function Testimonial() {
       </div>
 
       <div className="flex justify-between px-28">
+        {/* First Card */}
         <div className="py-10">
-          <div className="bg-[#253053] text-white w-[391px] h-[475px] py-6 px-10 rounded-lg shadow-lg">
-            <div className="flex items-center space-x-3 mb-4">
-              <img
-                src="https://a.storyblok.com/f/293525/116x40/68c8932d45/oneims_logo.svg"
-                alt="OneIMS Logo"
-              />
-            </div>
-            <p className="text-4xl font-bold text-white mb-4">
-              <span className="text-[#08ba98]">25%</span> cost savings by
-              eliminating wasted tasks
-            </p>
-            <div className="text-lg space-y-6">
-              <div className="mt-10">
-                <span className="text-[#08ba98] font-bold text-[28px]">
-                  10% - 25%
-                </span>
-                <span className="ml-1 font-bold block">Project savings</span>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front bg-[#253053] text-white w-[391px] h-[475px] py-6 px-10 rounded-lg shadow-lg">
+                <div className="flex items-center space-x-3 mb-4">
+                  <img
+                    src="https://a.storyblok.com/f/293525/116x40/68c8932d45/oneims_logo.svg"
+                    alt="OneIMS Logo"
+                  />
+                </div>
+                <p className="text-4xl font-bold text-white mb-4">
+                  <span className="text-[#08ba98]">25%</span> cost savings by
+                  eliminating wasted tasks
+                </p>
+                <div className="text-lg space-y-6">
+                  <div className="mt-10">
+                    <span className="text-[#08ba98] font-bold text-[28px]">
+                      10% - 25%
+                    </span>
+                    <span className="ml-1 font-bold block">Project savings</span>
+                  </div>
+                  <hr className="border-[#94A3B8] my-2" />
+                  <div>
+                    <span className="text-[#08ba98] font-bold text-[28px]">
+                      25%
+                    </span>
+                    <span className="font-bold block">
+                      Cost reduction from bad hires
+                    </span>
+                  </div>
+                </div>
               </div>
-              <hr className="border-[#94A3B8] my-2" />
-              <div>
-                <span className="text-[#08ba98] font-bold text-[28px]">
-                  25%
-                </span>
-                <span className="font-bold block">
-                  Cost reduction from bad hires
-                </span>
+              <div className="flip-card-back bg-[#ffffff] text-[#636b83] w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
+              <div className="flex items-center space-x-3 mb-4">
+                  <img
+                    src="https://a.storyblok.com/f/293525/117x40/faf4fdb088/group-283.svg"
+                    alt="OneIMS Logo"
+                  />
+                </div>
+
+                <div className="mt-8 pb-10 ">
+                  <p className="text-[18px]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod veritatis minima molestiae quia deleniti aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, soluta.</p>
+                </div>
+
+                <div className="mt-28">
+                  <h3 className="font-semibold text-black">Solomi Triputji</h3>
+                  <p>Lorem ipsum dolor sit.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
+        {/* Second Card */}
         <div className="py-10">
-          <div className="bg-white w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
-            <div className="flex items-center space-x-3 mb-4">
-              <img
-                src="https://a.storyblok.com/f/293525/168x114/54f107a54f/bachv4tmzickvpdgj90c-1.png/m/filters:format(webp):quality(70)"
-                alt="Bach Logo"
-                className="w-16"
-              />
-            </div>
-            <p className="text-4xl font-bold text-[#253053] mb-4">
-              <span className="text-[#294dff]">30%</span> faster project
-              completion
-            </p>
-            <div className="text-lg space-y-6">
-              <div className="mt-10">
-                <span className="text-[18px]">Bookkeeping reduced</span>
-                <span className="text-[#294dff] font-bold text-[25px] block">
-                  1 week to 1-2 days
-                </span>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front bg-white w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
+                <div className="flex items-center space-x-3 mb-4">
+                  <img
+                    src="https://a.storyblok.com/f/293525/168x114/54f107a54f/bachv4tmzickvpdgj90c-1.png/m/filters:format(webp):quality(70)"
+                    alt="Bach Logo"
+                    className="w-16"
+                  />
+                </div>
+                <p className="text-4xl font-bold text-[#253053] mb-4">
+                  <span className="text-[#294dff]">30%</span> faster project
+                  completion
+                </p>
+                <div className="text-lg space-y-6">
+                  <div className="mt-10">
+                    <span className="text-[18px]">Bookkeeping reduced</span>
+                    <span className="text-[#294dff] font-bold text-[25px] block">
+                      1 week to 1-2 days
+                    </span>
+                  </div>
+                  <hr className="border-[#94A3B8] my-2" />
+                  <div>
+                    <span className="text-[#294dff] font-bold text-[28px]">
+                      Over 100% ROI
+                    </span>
+                    <span className="font-bold block text-base">
+                      By improving productivity and visibility
+                    </span>
+                  </div>
+                </div>
               </div>
-              <hr className="border-[#94A3B8] my-2" />
-              <div>
-                <span className="text-[#294dff] font-bold text-[28px]">
-                  Over 100% ROI
-                </span>
-                <span className="font-bold block text-base">
-                  By improving productivity and visibility
-                </span>
+              <div className="flip-card-back bg-[#ffffff] text-[#636b83] w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
+              <div className="flex items-center space-x-3 mb-4">
+              <img
+                    src="https://a.storyblok.com/f/293525/168x114/54f107a54f/bachv4tmzickvpdgj90c-1.png/m/filters:format(webp):quality(70)"
+                    alt="Bach Logo"
+                    className="w-16"
+                  />
+                </div>
+
+                <div className="mt-8 pb-10 ">
+                  <p className="text-[18px]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod veritatis minima molestiae quia deleniti aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, soluta.</p>
+                </div>
+
+                <div className="mt-28">
+                  <h3 className="font-semibold text-black">Solomi Triputji</h3>
+                  <p>Lorem ipsum dolor sit.</p>
+                </div>
               </div>
             </div>
           </div>
         </div>
 
+        {/* Third Card */}
         <div className="py-10">
-          <div className="bg-[#ffe7aa] text-white w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
-            <div className="flex items-center space-x-3 mb-4">
-              <img
-                src="https://a.storyblok.com/f/293525/115x31/0d1406e2bb/group-323.svg"
-                alt="Group Logo"
-              />
-            </div>
-            <p className="text-4xl font-bold text-[#253053] mb-4">
-              <span className="text-[#1f95ff]">10%</span> cost savings by
-              eliminating wasted tasks
-            </p>
-            <div className="text-lg space-y-6">
-              <div className="mt-10">
-                <span className="text-[#1f95ff] font-bold text-[30px]">
-                  10% - 25%
-                </span>
-                <span className="ml-1 mt-2 font-semibold block text-[#253053]">
-                  Project savings
-                </span>
+          <div className="flip-card">
+            <div className="flip-card-inner">
+              <div className="flip-card-front bg-[#ffe7aa] text-white w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
+                <div className="flex items-center space-x-3 mb-4">
+                  <img
+                    src="https://a.storyblok.com/f/293525/115x31/0d1406e2bb/group-323.svg"
+                    alt="Group Logo"
+                  />
+                </div>
+                <p className="text-4xl font-bold text-[#253053] mb-4">
+                  <span className="text-[#1f95ff]">10%</span> cost savings by
+                  eliminating wasted tasks
+                </p>
+                <div className="text-lg space-y-6">
+                  <div className="mt-10">
+                    <span className="text-[#1f95ff] font-bold text-[30px]">
+                      10% - 25%
+                    </span>
+                    <span className="ml-1 mt-2 font-semibold block text-[#253053]">
+                      Project savings
+                    </span>
+                  </div>
+                  <hr className="border-[#94A3B8] my-2" />
+                  <div>
+                    <span className="text-[#1f95ff] font-bold text-[25px]">
+                      100% confidence
+                    </span>
+                    <span className="font-bold block text-[#253053]">
+                      In running remote operations
+                    </span>
+                  </div>
+                </div>
               </div>
-              <hr className="border-[#94A3B8] my-2" />
-              <div>
-                <span className="text-[#1f95ff] font-bold text-[25px]">
-                  100% confidence
-                </span>
-                <span className="font-bold block text-[#253053]">
-                  In running remote operations
-                </span>
+              <div className="flip-card-back bg-[#ffffff] text-[#636b83] w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
+              <div className="flex items-center space-x-3 mb-4">
+              <img
+                    src="https://a.storyblok.com/f/293525/115x31/0d1406e2bb/group-323.svg"
+                    alt="Group Logo"
+                  />
+                </div>
+
+                <div className="mt-8 pb-10 ">
+                  <p className="text-[18px]">Lorem ipsum dolor sit, amet consectetur adipisicing elit. Quod veritatis minima molestiae quia deleniti aperiam. Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis, soluta.</p>
+                </div>
+
+                <div className="mt-28">
+                  <h3 className="font-semibold text-black">Solomi Triputji</h3>
+                  <p>Lorem ipsum dolor sit.</p>
+                </div>
               </div>
             </div>
           </div>
