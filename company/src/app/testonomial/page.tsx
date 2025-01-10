1d505846964fc6464b91408038216207f5a51902
Faizan Shaikh <faizansheikh8209@gmail.com>
2025-01-10 14:25:10 +0530

    new-design

--- company/src/app/testonomial/page.tsx
+++ company/src/app/testonomial/page.tsx
@@ -1,174 +1,156 @@
-import React from 'react';
-import {
- 
-  Star,
-  
-} from "lucide-react";
-
+import React from "react";
+import { Star, StarHalf } from "lucide-react";
 
 export default function Testimonial() {
-
-
-  const data=[
+  const data = [
     {
-      id:1,
-      img:"https://hubstaff.com/images/logos/getapp-dark.svg",
-      content:"4.6 out of 5 stars from 1500 reviews",
-      rating:4.6
-
+      id: 1,
+      img: "https://hubstaff.com/images/logos/getapp-dark.svg",
+      content: "4.6 out of 5 stars from 1500 reviews",
+      rating: 4.6,
     },
     {
-      id:2,
-      img:"https://hubstaff.com/images/logos/capterra-dark.svg",
-      content:"4.6 out of 5 stars from 1500 reviews",
-      rating:4.6
-
+      id: 2,
+      img: "https://hubstaff.com/images/logos/capterra-dark.svg",
+      content: "4.6 out of 5 stars from 1500 reviews",
+      rating: 4.6,
     },
     {
-      id:3,
-      img:"https://hubstaff.com/images/logos/g2-dark.svg",
-      content:"4.5 out of 5 stars from 1500 reviews",
-      rating:4.5
-
+      id: 3,
+      img: "https://hubstaff.com/images/logos/g2-dark.svg",
+      content: "4.5 out of 5 stars from 1500 reviews",
+      rating: 4.5,
     },
-  ]
+  ];
 
+  const renderStars = (rating) => {
+    const fullStars = Math.floor(rating);
+    const halfStar = rating % 1 >= 0.5;
+    const emptyStars = 5 - Math.ceil(rating);
 
-  
+    return (
+      <div className="flex">
+        {[...Array(fullStars)].map((_, index) => (
+          <Star key={`full-${index}`} className="text-yellow-400 w-4 " />
+        ))}
+        {halfStar && <StarHalf className="text-yellow-400 w-4" />}
+        {[...Array(emptyStars)].map((_, index) => (
+          <Star key={`empty-${index}`} className="text-gray-300 " />
+        ))}
+      </div>
+    );
+  };
 
   return (
-    <div className='bg-[#f2f8ff] my-4 '>
-      <div className='text-center py-10'>
-        <p className='font-medium text-lg text-[#294dff]'>
+    <div className="bg-[#f2f8ff] my-4 ">
+      <div className="text-center py-10">
+        <p className="font-medium text-lg text-[#294dff]">
           Time tracking tools to maximize your team’s productivity
         </p>
-        <h3 className='text-[46px] font-medium text-[#253053]'>
+        <h3 className="text-[46px] font-medium text-[#253053]">
           See what our customers say
         </h3>
       </div>
 
-      <div className='flex justify-between '> 
-      <div className='px-24 py-10 flex justify-between '>
-      <div className="bg-[#1E293B] text-white w-[331px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
-      <div className="flex items-center space-x-3 mb-4">
-        <div className="bg-[#61DAFB] p-2 rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-white"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M10 3a7 7 0 107 7h-4a3 3 0 11-3-3V3z" />
-            <path d="M7 7a3 3 0 11-3-3v4a7 7 0 003 3z" />
-          </svg>
+      <div className="flex justify-between px-28">
+        <div className=" py-10  ">
+          <div className="bg-[#253053] text-white w-[391px] h-[475px] py-6 px-10 rounded-lg shadow-lg">
+            <div className="flex items-center space-x-3 mb-4">
+             <img src="https://a.storyblok.com/f/293525/116x40/68c8932d45/oneims_logo.svg" alt="" />
+            </div>
+            <p className="text-4xl font-bold text-white mb-4">
+              <span className="text-[#08ba98]">25%</span> cost savings by eliminating wasted tasks
+            </p>
+            <div className="text-lg space-y-6">
+              <div className="mt-10">
+                <span className="text-[#08ba98] font-bold text-[28px] ">
+                  10% - 25%
+                </span>
+                <span className="ml-1 font-bold block ]">Project savings</span>
+              </div>
+              <hr className="border-[#94A3B8] my-2" />
+              <div>
+                <span className="text-[#08ba98] font-bold text-[28px]">
+                  25%
+                </span>
+                <span className="font-bold block">
+                  Cost reduction from bad hires
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-        <h2 className="text-xl font-bold">OneIMS</h2>
-      </div>
-      <p className="text-3xl font-bold text-[#38BDF8] mb-4">
-        25% cost savings by eliminating wasted tasks
-      </p>
-      <div className="text-lg space-y-6">
-        <div className='mt-10'>
-          <span className="text-[#38BDF8] font-bold text-[30px] ">10% - 25%</span>
-          <span className="ml-1 font-bold block ]">Project savings</span>
+        
+        <div className=" py-10 ">
+          <div className="bg-white w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
+          <div className="flex items-center space-x-3 mb-4">
+             <img src="https://a.storyblok.com/f/293525/168x114/54f107a54f/bachv4tmzickvpdgj90c-1.png/m/filters:format(webp):quality(70)" alt="" className="w-16" />
+            </div>
+            <p className="text-4xl font-bold text-[#253053] mb-4">
+             <span className="text-[#294dff]"> 30% </span> faster project complition
+            </p>
+            <div className="text-lg space-y-6">
+              <div className="mt-10">
+                <span className="  text-[18px] ">
+                  Book keeping reduced
+                </span>
+                <span className=" text-[#294dff] font-bold text-[25px] block ]">1 week to 1-2 days</span>
+              </div>
+              <hr className="border-[#94A3B8] my-2" />
+              <div>
+                <span className="text-[#294dff] font-bold text-[28px] mt-4 ">
+                  Over 100% ROI
+                </span>
+                <span className="font-bold block text-base mt-4">
+                 By Improving produnctivty and visiblity
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-        <hr className="border-[#94A3B8] my-2" />
-        <div>
-          <span className="text-[#38BDF8] font-bold text-[30px]">25%</span>
-          <span className="font-bold block">Cost reduction from bad hires</span>
+        <div className=" py-10 ">
+          <div className="bg-[#ffe7aa] text-white w-[391px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
+            <div className="flex items-center space-x-3 mb-4">
+              
+              <img src="https://a.storyblok.com/f/293525/115x31/0d1406e2bb/group-323.svg" alt="" />
+            </div>
+            <p className="text-4xl font-bold text-[#253053] mb-4">
+              <span className="text-[#1f95ff]">10%</span> cost savings by eliminating wasted tasks
+            </p>
+            <div className="text-lg space-y-6">
+              <div className="mt-10">
+                <span className="text-[#1f95ff] font-bold text-[30px]  ">
+                  10% - 25%
+                </span>
+                <span className="ml-1  mt-2 font-semibold block  text-[#253053]">Project savings</span>
+              </div>
+              <hr className="border-[#94A3B8] my-2" />
+              <div>
+                <span className="text-[#1f95ff] font-bold text-[25px]">
+                  100% confidenece
+                </span>
+                <span className="font-bold block text-[#253053]">
+                  In running a remote opertains
+                </span>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
-      </div>
-      <div className='px-24 py-10 flex justify-between'>
-      <div className="bg-[#1E293B] text-white w-[331px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
-      <div className="flex items-center space-x-3 mb-4">
-        <div className="bg-[#61DAFB] p-2 rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-white"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M10 3a7 7 0 107 7h-4a3 3 0 11-3-3V3z" />
-            <path d="M7 7a3 3 0 11-3-3v4a7 7 0 003 3z" />
-          </svg>
-        </div>
-        <h2 className="text-xl font-bold">OneIMS</h2>
-      </div>
-      <p className="text-3xl font-bold text-[#38BDF8] mb-4">
-        25% cost savings by eliminating wasted tasks
-      </p>
-      <div className="text-lg space-y-6">
-        <div className='mt-10'>
-          <span className="text-[#38BDF8] font-bold text-[30px] ">10% - 25%</span>
-          <span className="ml-1 font-bold block ]">Project savings</span>
-        </div>
-        <hr className="border-[#94A3B8] my-2" />
-        <div>
-          <span className="text-[#38BDF8] font-bold text-[30px]">25%</span>
-          <span className="font-bold block">Cost reduction from bad hires</span>
-        </div>
-      </div>
-    </div>
-      </div>
-      <div className='px-24 py-10 flex justify-between'>
-      <div className="bg-[#1E293B] text-white w-[331px] h-[475px] py-6 px-8 rounded-lg shadow-lg">
-      <div className="flex items-center space-x-3 mb-4">
-        <div className="bg-[#61DAFB] p-2 rounded-full">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6 text-white"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-          >
-            <path d="M10 3a7 7 0 107 7h-4a3 3 0 11-3-3V3z" />
-            <path d="M7 7a3 3 0 11-3-3v4a7 7 0 003 3z" />
-          </svg>
-        </div>
-        <h2 className="text-xl font-bold">OneIMS</h2>
-      </div>
-      <p className="text-3xl font-bold text-[#38BDF8] mb-4">
-        25% cost savings by eliminating wasted tasks
-      </p>
-      <div className="text-lg space-y-6">
-        <div className='mt-10'>
-          <span className="text-[#38BDF8] font-bold text-[30px] ">10% - 25%</span>
-          <span className="ml-1 font-bold block ]">Project savings</span>
-        </div>
-        <hr className="border-[#94A3B8] my-2" />
-        <div>
-          <span className="text-[#38BDF8] font-bold text-[30px]">25%</span>
-          <span className="font-bold block">Cost reduction from bad hires</span>
-        </div>
-      </div>
-    </div>
-      </div>
       </div>
 
-    <div className='max-w-4xl mx-auto py-20 flex gap-10'>
-
-      {
-        data.map((itrm)=>{
-          return (<>
-             <div key={itrm.id} >
-      <img src={itrm.img} />
-      <p>{itrm.content}</p>
-      <Star/>
-    </div>
-          </>)
-        })
-      }
- 
-    </div>
-      
+      <div className="max-w-4xl mx-auto py-20 flex gap-14">
+        {data.map((itrm) => {
+          return (
+            <>
+              <div key={itrm.id}>
+                <img src={itrm.img} className="mb-3" />
+                <p className="text-base mb-3">{itrm.content}</p>
+                <p className="ml-10">{renderStars(itrm.rating)}</p>
+              </div>
+            </>
+          );
+        })}
+      </div>
     </div>
   );
 }
-
-
-
-
-
-
-
